e2a2236cd3eb0fe92e80ef45891576ebb70c7b1d
duwaar <duwaar@yahoo.com>
2020-04-26 13:27:12 -0700

    Moved csv reading code into its own function.

--- import.py
+++ import.py
@@ -1,11 +1,19 @@
 from csv import reader
+import sqlalchemy
+
+
+def get_csv_rows(filename):
+    csvfile = open(filename)
+    csvreader = reader(csvfile)
+    for row in csvreader:
+        yield row
 
 
 def main():
-    csvfile = open('books.csv')
-    csvreader = reader(csvfile)
-    for row in csvreader:
+    for row in get_csv_rows('books.csv'):
         print(row)
+    
+
 
 
 if __name__ == "__main__":
