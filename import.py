c2b1cb2e24aeefb6e63fabcc91e9e0eae7436b9f
duwaar <duwaar@yahoo.com>
2020-04-27 10:12:32 -0700

    Added code to insert new rows into database. Need a function for escaping strings.

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 from csv import reader
-import sqlalchemy
+from sqlalchemy import create_engine
+from os import getenv
 
 
 def get_csv_rows(filename):
@@ -8,12 +9,30 @@ def get_csv_rows(filename):
     for row in csvreader:
         yield row
 
+def connect_database(db_url=None):
+    if db_url == None:
+        db_url = getenv('DATABASE_URL')
+    engine = create_engine(db_url)
+    return engine
+
+def escape(string):
+    pass
+
 
 def main():
-    for row in get_csv_rows('books.csv'):
-        print(row)
+    engine = connect_database('postgres://qhutchfxohvldy:06fb88a98fcb777f47b0cf6c3dd38bf8c6ccc0cd22cb540da4e607bd0b431bd8@ec2-54-197-34-207.compute-1.amazonaws.com:5432/d567u6iinhno1a')
     
-
+    #with engine.begin() as connection:
+        #connection.execute('CREATE TABLE books (id SERIAL PRIMARY KEY, isbn INTEGER, title TEXT NOT NULL, author TEXT, year INTEGER);') 
+    
+    with engine.begin() as connection:
+        for i, row in enumerate(get_csv_rows('books.csv')):
+            if i > 0:
+                escaped_row = []
+                for field in row:
+                    escaped_row.append(escape(field))
+                isbn, title, author, year = escaped_row
+                connection.execute('INSERT INTO books (isbn, title, author, year) VALUES ({}, {}, {}, {});'.format(isbn, title, author, year))
 
 
 if __name__ == "__main__":
