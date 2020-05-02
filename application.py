8e7a569a4e99fcf1e524601c6eef0662ec5cf4c2
duwaar <duwaar@yahoo.com>
2020-05-01 18:01:00 -0700

    Added search functionality.

--- application.py
+++ application.py
@@ -7,21 +7,36 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv('DATABASE_URL'):
-    raise RuntimeError('DATABASE_URL is not set')
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route('/')
-def home_page(query=''):
-    query = request.args.get('query')
-    return render_template('home.html', query=query)
+@app.route("/")
+def home_page():
+    search_by = request.args.get("search_by")
+    query = request.args.get("query")
+
+    if query:
+        if search_by == "title"\
+                or search_by == "author"\
+                or search_by == "isbn":
+            results = db.execute(f"SELECT * FROM books WHERE {search_by} ILIKE '%{query}%';").fetchall()
+        elif search_by == "year":
+            results = db.execute(f"SELECT * FROM books WHERE {search_by} = {query};").fetchall()
+        else:
+            pass
+            #redirect(error_page(f"Illegal query: {search_by}")
+    else:
+        results = []
+    
+    return render_template("home.html", query=query, results=results)
 
