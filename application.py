998a34dbcb1cbbc8aa823788cdafa486407b4b8c
duwaar <duwaar@yahoo.com>
2020-05-04 16:05:18 -0700

    Added a book info page.

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-import os
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
 app = Flask(__name__)
 
@@ -20,7 +20,7 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home_page():
     search_by = request.args.get("search_by")
     query = request.args.get("query")
@@ -32,39 +32,46 @@ def home_page():
             results = db.execute(f"SELECT * FROM books WHERE {search_by} ILIKE '%{query}%';").fetchall()
         elif search_by == "year":
             results = db.execute(f"SELECT * FROM books WHERE {search_by} = {query};").fetchall()
-        else:
-            pass
-            #redirect(error_page(f"Illegal query: {search_by}")
     else:
         results = []
     
     return render_template("home.html", query=query, results=results)
 
+def valid_login(username, password):
+    account = db.execute(f"SELECT username, pw_hash FROM users WHERE username = '{username}'").fetchone()
+    if password == account[1]:
+        return True
+    else:
+        return False
+
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+        if valid_login(request.form.get("username"), request.form.get("password")):
+            session["username"] = request.form.get("username")
+            message = f"Welcome back to BookReader, {session.get('username')}!"
+        else:
+            message = "Invalid login."
     else:
-        username = ""
+        message = "Enter your login information."
 
-    return render_template("login.html", success=False, username=username)
+    return render_template("login.html", message=message)
 
 def check_reg_form(form_data):
     success = False
-    if form_data["email"] == "":
+    if form_data.get("email") == "":
         message = "You must enter an email address."
-    elif len(form_data["username"]) < 5\
-            or len(form_data["username"]) > 20:
+    elif len(form_data.get("username")) < 5\
+            or len(form_data.get("username")) > 20:
         message = "Username must be between 5 and 20 characters long."
-    elif db.execute(f"SELECT id FROM users WHERE username = '{form_data['username']}';").fetchall():
-        message = f"An account with the username \"{form_data['username']}\" already exists."
-    elif len(form_data["password"]) < 6\
-            or len(form_data["password"]) > 50:
+    elif db.execute(f"SELECT id FROM users WHERE username = '{form_data.get('username')}';").fetchall():
+        message = f"An account with the username \"{form_data.get('username')}\" already exists."
+    elif len(form_data.get("password")) < 6\
+            or len(form_data.get("password")) > 50:
         message = "Password must be between 6 and 50 characters long."
-    elif form_data["password"] == form_data["username"]:
+    elif form_data.get("password") == form_data.get("username"):
         message = "Please do not make your password the same as your username."
-    elif form_data["password"] != form_data["re_pword"]:
+    elif form_data.get("password") != form_data.get("re_pword"):
         message = "You re-typed your password incorrectly."
     else:
         success = True
@@ -78,13 +85,29 @@ def register_page():
         reg_valid, err_message = check_reg_form(request.form)
         if reg_valid:
             db.execute(f"INSERT INTO users (username, pw_hash, email) VALUES (:username, :pw_hash, :email);",\
-                    {"username":request.form["username"], "pw_hash":request.form["password"], "email":request.form["email"]})
+                    {"username":request.form.get("username"), "pw_hash":request.form.get("password"), "email":request.form.get("email")})
             db.commit()
-            #user_id = db.execute(f"SELECT id FROM users WHERE username = '{request.form['username']}';").fetchall()[0][0]
-            message = f"Yay! You have registered as {request.form['username']}. Welcome to Bookreader!"
+            #user_id = db.execute(f"SELECT id FROM users WHERE username = '{request.form.get('username')}';").fetchall()[0][0]
+            message = f"Yay! You have registered as {request.form.get('username')}. Welcome to Bookreader!"
         else:
             message = err_message
     else:
         message = "Please fill out the form."
 
     return render_template("register.html", message=message)
+
+@app.route("/book/<string:book_isbn>", methods=["GET"])
+def book_page(book_isbn):
+    db_result = db.execute(f"SELECT title, author, year, isbn FROM books WHERE isbn = '{book_isbn}';").fetchone()
+    book_data = {'title':'', 'author':'', 'year':'', 'isbn':''}
+    if db_result:
+        book_data['title'] = db_result[0]
+        book_data['author'] = db_result[1]
+        book_data['year'] = db_result[2]
+        book_data['isbn'] = db_result[3]
+
+    return render_template("book.html", book_data=book_data)
+
+@app.route("/submit_read", methods=["GET"])
+def submit_read():
+    return "To do."
